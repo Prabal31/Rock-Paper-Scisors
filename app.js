29d03106327b782e35f491066a5a302d93eaae26
Prabal <manchandaprabal878@gmail.com>
2024-06-26 18:40:19 -0400

    chore: Update server port to 3000 for consistency

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+const fs = require('fs');
+// TODO: Require the http module
 const http=require('http');
 
-const server=http.createServer((req,res)=>{
-  res.end("Running");
+const server = http.createServer((req, res) => {
+  res.end('Server is running!');
 });
 
-server.listen(4001, () => {
-  const { address, port } = server.address();
-  console.log(`Server is listening on: http://${address}:${port}`);
-})
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
