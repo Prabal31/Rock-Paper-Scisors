f3504f4508f36d7e9879b5b8836284b6faea961a
Prabal <manchandaprabal878@gmail.com>
2024-06-26 18:43:06 -0400

    chore: Update server port to 3000 for consistency

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const http=require('http');
 
 const server = http.createServer((req, res) => {
   res.end('Server is running!');
+  const url=new URL(req.url,`http://${request.headers.host}`);
 });
 
 server.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
+  
 });
